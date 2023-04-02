820b8594e3ead56212dfd0753097175a7967558a
samedovAV <alekcandr206@gmail.com>
2023-04-02 14:55:58 +0300

    basic methods in service class

--- backend/src/main/java/com/example/backend/domain/Article.java
+++ backend/src/main/java/com/example/backend/domain/Article.java
@@ -1,4 +1,24 @@
 package com.example.backend.domain;
 
-public class Article {
+import java.io.Serial;
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String title;
+    private String description;
+    private String authorName;
+
 }
