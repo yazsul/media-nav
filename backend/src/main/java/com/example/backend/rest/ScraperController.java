1dbbc743a3e644c02dcf52f2e0ef382343b16f35
samedovAV <alekcandr206@gmail.com>
2023-04-02 14:48:14 +0300

    basic methods refactor

--- backend/src/main/java/com/example/backend/rest/ScraperController.java
+++ backend/src/main/java/com/example/backend/rest/ScraperController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import com.example.backend.domain.Article;
 import com.example.backend.service.ScraperService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/articles")
@@ -17,25 +14,25 @@ public class ScraperController {
     @Autowired
     ScraperService scraperService;
 
-    @RequestMapping(value="/by-author/{authorName}", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(value="/by-author/{authorName}", produces = "application/json")
     public List<Article> searchArticlesByAuthor(@PathVariable("authorName") String authorName) {
         return scraperService.searchArticlesByAuthor(authorName);
     }
 
     //List all the authors
-    @RequestMapping(value="/authors", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(value="/authors", produces = "application/json")
     public List<String> listAuthors() {
         return scraperService.listAuthors();
     }
 
     //Search articles by title
-    @RequestMapping(value="/by-title/{title}", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(value="/by-title/{title}", produces = "application/json")
     public List<Article> searchArticleByTitle(@PathVariable("title") String title) {
         return scraperService.searchArticleByTitle(title);
     }
 
     //search articles by description
-    @RequestMapping(value="/by-desc/{desc}", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(value="/by-desc/{desc}", produces = "application/json")
     public List<Article> searchArticleByDescription(@PathVariable("desc") String desc) {
         return scraperService.searchArticleByDescription(desc);
     }
