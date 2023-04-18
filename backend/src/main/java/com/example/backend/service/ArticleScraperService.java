ed960ddc94cfeea40bb2e8389dd4bd83f532acb6
samedovAV <alekcandr206@gmail.com>
2023-04-18 21:26:30 +0300

    finish with skeleton with hardcoded properties

--- backend/src/main/java/com/example/backend/service/ArticleScraperService.java
+++ backend/src/main/java/com/example/backend/service/ArticleScraperService.java
@@ -1,12 +1,16 @@
 package com.example.backend.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.example.backend.domain.Article;
 
 import java.util.stream.Collectors;
 
+import com.example.backend.util.ScraperHelper;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,24 +41,37 @@ public class ArticleScraperService {
     @Value("#{'${newspaper.thehindu.article.searchtags}'.split(',')}")
     List<String> articleLinksSearchTags;
 
+    @PostConstruct
+    public void loadContents() {
+        LOGGER.info("loadContents()...start");
+        articles.clear();
+        List<String> articleDetailsSearchTags = Arrays.asList(authorTagName, titleTagName, descTagName);
+        ScraperHelper scraperHelper = new ScraperHelper(newspaperUrl, parseTimeoutMillis, articleDetailsSearchTags, articleLinksSearchTags);
+
+        LOGGER.info("Extracting article details...start");
+
+        scraperHelper.fetchAllLinkMetaDetailsFromPage()
+                .thenAccept(list-> list.stream().filter(map->map.get(authorTagName)!=null && map.get(authorTagName).length()>0)
+                        .forEach(map-> articles.add(new Article(map.get(titleTagName), map.get(descTagName), map.get(authorTagName)))));
+
+        LOGGER.info("loadContents()...completed");
+    }
+
     public List<String> listAuthors() {
-        return articles.stream().map(a->a.getAuthorName())
+        return articles.stream().map(Article::getAuthorName)
                 .distinct()
                 .collect(Collectors.toList());
     }
 
     public List<Article> searchArticlesByAuthor(String authorName) {
-        return articles.stream().filter(a->a.getAuthorName().equalsIgnoreCase(authorName))
-                .collect(Collectors.toList());
+        return articles.stream().filter(a->a.getAuthorName().equalsIgnoreCase(authorName)).toList();
     }
 
     public List<Article> searchArticleByTitle(String title) {
-        return articles.stream().filter(a->a.getTitle().startsWith(title))
-                .collect(Collectors.toList());
+        return articles.stream().filter(a->a.getTitle().startsWith(title)).toList();
     }
 
     public List<Article> searchArticleByDescription(String desc) {
-        return articles.stream().filter(a->a.getDescription().startsWith(desc))
-                .collect(Collectors.toList());
+        return articles.stream().filter(a->a.getDescription().startsWith(desc)).toList();
     }
 }
