186f18b34ed840b68e7c1a2e2b58bec9b91ab10d
samedovAV <alekcandr206@gmail.com>
2023-04-18 21:26:30 +0300

    custom url way

---
+++ backend/src/test/java/com/example/backend/ScraperControllerIT.java
@@ -0,0 +1,55 @@
+package com.example.backend;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.example.backend.domain.Article;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ScraperControllerIT {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") // idea bug
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAtriclesInvalidUrl() throws Exception {
+        String url = "https://example.com"; // URL to be passed as a path variable
+        // Perform a GET request to the endpoint with the URL as a path variable
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/articles/from-url?url="+url))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+        // Add more assertions for the response as needed
+    }
+
+    @Test
+    public void testGetRealArticle() throws Exception {
+        String url = "https://www.nytimes.com/2023/05/07/world/europe/netherlands-ajax-antisemitism.html";
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/articles/from-url?url="+url))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Article> article = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(article.get(0));
+    }
+}
