6bc432af5b29ab032272783b7908e79e17e4da47
samedovAV <alekcandr206@gmail.com>
2023-06-08 18:31:01 +0300

    Add tests

--- backend/src/test/java/com/example/backend/ScraperControllerIT.java
+++ backend/src/test/java/com/example/backend/ScraperControllerIT.java
@@ -1,8 +1,7 @@
 package com.example.backend;
 
-import java.lang.reflect.Type;
-import java.util.List;
-
+import com.example.backend.communication.GatewayRequest;
+import com.example.backend.communication.GatewayResponse;
 import com.example.backend.domain.Article;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,15 +10,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -60,5 +62,157 @@ public class ScraperControllerIT {
         Article article = objectMapper.readValue(response, new TypeReference<>(){});
 
         assertNotNull(article);
+    }
+
+    @Test
+    public void testCallScrapingService() throws Exception {
+        String url = "https://www.nytimes.com/2023/05/07/world/europe/netherlands-ajax-antisemitism.html";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> body = new HashMap<>();
+        body.put("requestForScrappingService", url);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/call-scraping-service")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(body))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        GatewayResponse gatewayResponse = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(gatewayResponse.getResponse());
+    }
+
+    @Test
+    public void testPredictTonality() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> body = new HashMap<>();
+        body.put("requestForDataAnalysisService", "An example sentence.");
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/predict-tonality")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        GatewayResponse gatewayResponse = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(gatewayResponse.getResponse());
+    }
+
+    @Test
+    public void testPredictLeaning() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> body = new HashMap<>();
+        body.put("requestForDataAnalysisService", "An example sentence.");
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/predict-leaning")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        GatewayResponse gatewayResponse = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(gatewayResponse.getResponse());
+    }
+
+    @Test
+    public void testWordCloud() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> body = new HashMap<>();
+        body.put("requestForDataAnalysisService", "It seems like black cherry and full-bodied are the most mentioned " +
+                "characteristics, and Cabernet Sauvignon is the most popular of them all. This aligns with the fact that " +
+                "Cabernet Sauvignon \\\"is one of the world's most widely recognized red wine grape varieties. " +
+                "It is grown in nearly every major wine-producing country among a diverse spectrum of climates " +
+                "from Canada's Okanagan Valley to Lebanon's Beqaa Valley");
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/wordcloud")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        GatewayResponse gatewayResponse = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(gatewayResponse.getResponse());
+    }
+
+    @Test
+    public void testScrapeArticleAndPredictTonality() throws Exception {
+        String url = "https://www.nytimes.com/2023/05/07/world/europe/netherlands-ajax-antisemitism.html";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> body = new HashMap<>();
+        body.put("requestForScrappingService", url);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/scrape-article-and-predict-tonality")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        GatewayResponse gatewayResponse = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(gatewayResponse.getResponse());
+    }
+
+    @Test
+    public void testScrapeArticleAndPredictLeaning() throws Exception {
+        String url = "https://www.nytimes.com/2023/05/07/world/europe/netherlands-ajax-antisemitism.html";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> body = new HashMap<>();
+        body.put("requestForScrappingService", url);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/scrape-article-and-predict-leaning")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        GatewayResponse gatewayResponse = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(gatewayResponse.getResponse());
+    }
+
+    @Test
+    public void testScrapeArticleAndGetWordCloud() throws Exception {
+        String url = "https://www.nytimes.com/2023/05/07/world/europe/netherlands-ajax-antisemitism.html";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> body = new HashMap<>();
+        body.put("requestForScrappingService", url);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/scrape-article-and-get-wordcloud")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(body))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+
+        GatewayResponse gatewayResponse = objectMapper.readValue(response, new TypeReference<>(){});
+
+        assertNotNull(gatewayResponse.getResponse());
     }
 }
